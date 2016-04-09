74772ba2b9bb516773484c251e01060a997f6a3b
Ziyan <jaraidaniel@gmail.com>
2016-04-09 13:10:06 +0200

    espresso: remove unused blobs
    Change-Id: I97dd385492be7df479b6afc1a700db006a5298dc

--- espresso-common/common-vendor-blobs.mk
+++ espresso-common/common-vendor-blobs.mk
@@ -22,10 +22,8 @@ PRODUCT_COPY_FILES += \
 # Wi-Fi
 PRODUCT_COPY_FILES += \
     $(VENDOR_FOLDER)/proprietary/system/etc/wifi/bcmdhd_apsta.bin:system/etc/wifi/bcmdhd_apsta.bin \
-    $(VENDOR_FOLDER)/proprietary/system/etc/wifi/bcmdhd_mfg.bin:system/etc/wifi/bcmdhd_mfg.bin \
     $(VENDOR_FOLDER)/proprietary/system/etc/wifi/bcmdhd_p2p.bin:system/etc/wifi/bcmdhd_p2p.bin \
     $(VENDOR_FOLDER)/proprietary/system/etc/wifi/bcmdhd_sta.bin:system/etc/wifi/bcmdhd_sta.bin \
-    $(VENDOR_FOLDER)/proprietary/system/etc/wifi/nvram_mfg.txt:system/etc/wifi/nvram_mfg.txt \
     $(VENDOR_FOLDER)/proprietary/system/etc/wifi/nvram_net.txt:system/etc/wifi/nvram_net.txt \
     $(VENDOR_FOLDER)/proprietary/system/vendor/firmware/BCM4330.hcd:system/vendor/firmware/BCM4330.hcd
 
