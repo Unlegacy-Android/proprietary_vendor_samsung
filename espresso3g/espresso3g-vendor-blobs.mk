bb3ec067129e8e833dc7555dffd846103a7efb6f
Andreas Blaesius <skate4life@gmx.de>
2017-03-26 14:20:31 +0200

    espresso3g: move cbd to system/vendor/bin
    Change-Id: I12cac6f2a1c02f632a31201c7f1b795a6cdf4d76

--- espresso3g/espresso3g-vendor-blobs.mk
+++ espresso3g/espresso3g-vendor-blobs.mk
@@ -2,5 +2,5 @@ VENDOR_FOLDER := vendor/samsung/espresso3g
 
 # RIL
 PRODUCT_COPY_FILES += \
-    $(VENDOR_FOLDER)/proprietary/sbin/cbd:root/sbin/cbd \
+    $(VENDOR_FOLDER)/proprietary/vendor/bin/cbd:system/vendor/bin/cbd \
     $(VENDOR_FOLDER)/proprietary/vendor/lib/libsec-ril.so:system/vendor/lib/libsec-ril.so
